cd8909594cb47aa2a34493454d3666d98f7cdc2d
Thanh Ngọc Nguyễn Đức <ngochc1@gmail.com>
2021-08-31 14:49:41 +0700

    second commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
+    res.send({ hi: 'thereeeeeeeee' });
 });
 const PORT = process.env.PORT||5000;
 app.listen(PORT);
