8c440af82e0764fc948076fe46a55598a09f2407
Thanh Ngọc Nguyễn Đức <ngochc1@gmail.com>
2021-09-01 16:55:55 +0700

    finished auth flow

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 const express = require('express');
-const app = express();
-app.get('/', (req, res) => {
-    res.send({ hi: 'thereeeeeeeee' });
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+require('./model/User')
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
-const PORT = process.env.PORT||5000;
+
+const app = express();
+
+app.use(cookieSession({
+    maxAge:30*24*60*60*1000,
+    keys:[keys.cookieKey]
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT);
---
+++ services/passport.js
@@ -0,0 +1,30 @@
+const passport = require('passport');
+const GoogleStategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys')
+const mongoose = require('mongoose');
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+})
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => { done(null, user) })
+})
+passport.use(new GoogleStategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+    User.findOne({ googleId: profile.id })
+        .then((user) => {
+            if (user) {
+                done(null, user);
+            }
+            else {
+                new User({ googleId: profile.id }).save()
+                    .then((user) => done(null, user))
+            }
+
+        })
+}));
