ad164fae00cc084d7fba876e96b50c884860a8e1
Thanh Ngọc Nguyễn Đức <ngochc1@gmail.com>
2021-09-01 17:07:34 +0700

    tweak google strategy setting

--- services/passport.js
+++ services/passport.js
@@ -14,7 +14,8 @@ passport.deserializeUser((id, done) => {
 passport.use(new GoogleStategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
+    callbackURL: '/auth/google/callback',
+    proxy:true
 }, (accessToken, refreshToken, profile, done) => {
     User.findOne({ googleId: profile.id })
         .then((user) => {
